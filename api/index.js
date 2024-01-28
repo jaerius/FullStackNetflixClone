4f2b0c017849447fee5c5b6890dae6b96f47d8f7
jaerius <rylynn1029@naver.com>
2024-01-28 19:14:53 +0900

    netflix full stack app clone start

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+const app = express();
+
+// Mongoose connection without the deprecated options
+mongoose.connect(process.env.MONGO_URL, {
+    connectTimeoutMS: 30000, // Increase timeout to 30 seconds
+    socketTimeoutMS: 45000
+})
+.then(() => {
+    console.log("Connected to MongoDB via Mongoose");
+
+    // Middleware
+    app.use(express.json());
+    app.use("/api/auth", authRoute);
+
+    // Start the Express server
+    app.listen(8800, () => {
+        console.log("Backend server is running");
+    });
+})
+.catch(err => {
+    console.error("MongoDB connection error:", err);
+});
